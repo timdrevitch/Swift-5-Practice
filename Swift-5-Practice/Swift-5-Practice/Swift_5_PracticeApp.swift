4ab06dbe4482f7e24b3a57242229a66a9208ed3f
timdrevitch <110933291+timdrevitch@users.noreply.github.com>
2023-11-02 15:23:24 -0400

    initial commit

---
+++ Swift-5-Practice/Swift-5-Practice/Swift_5_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_5_PracticeApp.swift
+//  Swift-5-Practice
+//
+//  Created by ogre drev on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_5_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
